d65e0cb9c7734b4de456a3f9d948f1183a4d69b6
xenodium <8107219+xenodium@users.noreply.github.com>
2025-09-26 12:39:05 +0100

    Bumping version

--- agent-shell.el
+++ agent-shell.el
@@ -4,9 +4,9 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/agent-shell
-;; Version: 0.1.6
+;; Version: 0.1.7
 
-(defconst agent-shell--version "0.1.6")
+(defconst agent-shell--version "0.1.7")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
