0fbce98d9a96a432029227ad4ea8ce6714d1ea4f
xenodium <8107219+xenodium@users.noreply.github.com>
2025-10-22 18:20:12 +0100

    Bumping version #90

--- agent-shell.el
+++ agent-shell.el
@@ -4,10 +4,10 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/agent-shell
-;; Version: 0.10.2
+;; Version: 0.10.3
 ;; Package-Requires: ((shell-maker "0.82.2")(acp "0.6.1"))
 
-(defconst agent-shell--version "0.10.2")
+(defconst agent-shell--version "0.10.3")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
