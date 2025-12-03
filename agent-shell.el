51efb7d3406c3e3f4de1a2f3294fa98d6265e46d
xenodium <8107219+xenodium@users.noreply.github.com>
2025-12-03 17:48:44 +0000

    Bumping acp.el version required #127

--- agent-shell.el
+++ agent-shell.el
@@ -4,10 +4,10 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/agent-shell
-;; Version: 0.18.2
-;; Package-Requires: ((emacs "29.1") (shell-maker "0.84.1") (acp "0.7.3"))
+;; Version: 0.19.1
+;; Package-Requires: ((emacs "29.1") (shell-maker "0.84.1") (acp "0.8.1"))
 
-(defconst agent-shell--version "0.18.2")
+(defconst agent-shell--version "0.19.1")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -1379,8 +1379,8 @@ Set NO-FOCUS to start in background.
 Set NEW-SESSION to start a separate new session."
   (unless (version<= "0.84.1" shell-maker-version)
     (error "Please update shell-maker to version 0.84.1 or newer"))
-  (unless (version<= "0.7.3" acp-package-version)
-    (error "Please update acp.el to version 0.7.3 or newer"))
+  (unless (version<= "0.8.1" acp-package-version)
+    (error "Please update acp.el to version 0.8.1 or newer"))
   (with-temp-buffer ;; client-maker needs a buffer (use a temp one)
     (unless (and (map-elt config :client-maker)
                  (funcall (map-elt config :client-maker) (current-buffer)))
